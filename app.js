b929fda832a633fa6e39fe7646b8d7b4f88cb2b3
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-10 08:14:56 -0800

    takes markup and gives html back

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 "use strict"
 
 // serve the page the user views on port...
-var PORT = 3000; 
+var PORT = 3001; 
 
+// allows the use of express node library
+var express = require("express");
 // use this variable to employ "middleware" that is
 // needed to translate code to a format that is readable
 // to this app. 
@@ -10,12 +12,6 @@ var bodyParser = require('body-parser');
 var morgan = require("morgan");
 var marked = require("marked");
 
-
-var markupInput;
-
-
-// allows the use of express node library
-var express = require("express");
 //shorthand for invoking express-- since its going to
 // com up on almost every line
 var app = express();
@@ -25,29 +21,31 @@ app.set("view engine", "jade");
 
 //allows me to use public (static) files
 app.use(express.static('public'));
-
  // morgan dictates how req info is consoled
 app.use(morgan('dev'));
-// allows json to be used 
-app.use(bodyParser.json());
 // allows website? language
 app.use(bodyParser.urlencoded({extended: true}));
+// allows json to be used ... but I'm not using JSON
+// app.use(bodyParser.json());
 
 // on the home page render the index page (which will be in Jade)
-app.get("/marked", function(req, res){
+// this is initated as soon as the browser arrives at localhost 3001
+app.get("/", function(req, res){
 	res.render("index");
 
 })
 
-// get the info from the input field and convert it to express speak
+//third step...
+// the browser is requseting to post the input from the
+// input field (ie the body). the app responds by converting it to
+// mark up using the marked plugin and sends it back to the
+// main. js page 
 app.post("/marked", function(req, res){
+	var outputHTML = marked(req.body.inputText);  
+	res.send(outputHTML);  
+		console.log(req.body.inputText);
+});
 
-//	res.SEND  
-})
-
-// function convertToHTML(){
-// 	outPutHTML = marked(inputText);
-// }
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -3,27 +3,33 @@
 $(document).ready(init);
 
 function init () {
-	
 
 	$("#submit").on("click", sendMarkupInput);
-
 	
 }
 
-//send input from the input box to the express file
+//once app.js renders the jade index page 
+// they input text, click a button and...
+
 function sendMarkupInput () {
-	console.log("I'm in getMarkupInput")
-	var inputText = $("#text");
-	$.ajax("./app.js")
-		.done(function(data, status){
-			$.post(JSON.stringify(inputText));
-			console.log(JSON.stringify(inputText));
-debugger;
-		})
-		.fail(function(promise, status, error){
-			console.log("promise:", promise);
-			console.log("status:", status);
-			console.log("error:", error);
-		})
-		.get(JSON.parse())
+//send input from the input box to the express file
+// this is thing 2
+	var inputText = {inputText: $("#text").val()}
+	console.log("texty text text:" , inputText);
+	$.post("/marked",  inputText, function(){
+		console.log("got through to server");
+	})
+			.done(function(data){		
+				console.log(data);
+// 				//final step (4), take the converted markup comes back
+// // as data and is converted into html then
+// // given to the output field to use as its
+// // html...  
+ 			$("#output").empty().html($.parseHTML(data));
+		}) 
+	// })
+		// .fail(function(error){
+		// 	console.log("error:", error);
+		// });	
 }
+
